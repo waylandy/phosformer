38475b9d2111d48e8876695642ad3677c545ed4d
Wayland Yeung <waylandy2@gmail.com>
2022-11-07 13:57:05 -0500

    Add files via upload

---
+++ Phosformer/modules.py
@@ -0,0 +1,127 @@
+import sys
+import time 
+import numpy as np
+from scipy.special import softmax
+
+import torch
+
+def batch_job(kinases, substrates, model=None, tokenizer=None, input_order='SK', output_hidden_states=False, output_attentions=False, batch_size=20, device='cpu', threads=1, verbose=False):
+
+    if not callable(model) or not callable(tokenizer):
+        raise Exception('"model" and "tokenizer" must be provided as keyword arguments.')
+
+    torch.set_num_threads = threads
+    torch.cuda.empty_cache()
+    model.eval()
+
+    ############
+    token_dic   = {'.':'<mask>', '-':'<pad>'}
+    format_one  = lambda x: ''.join(token_dic[i] if i in token_dic else i.upper() for i in x if not i.isspace())
+    format_many = lambda x: list(map(format_one, x))
+
+    ############
+    start = time.time()
+    
+    total = len(kinases)
+    kinase_buffer, substrate_buffer = [], []
+    for n, (kinase, substrate) in enumerate(zip(kinases, substrates)):
+        kinase_buffer += [kinase]
+        substrate_buffer += [substrate]
+        if (n == total - 1) or ((n + 1) % batch_size == 0):
+            
+            ############
+            if input_order == 'SK':
+                ids = tokenizer(format_many(substrate_buffer), format_many(kinase_buffer), truncation=False, padding=True)
+                
+            if input_order == 'KS':
+                ids = tokenizer(format_many(kinase_buffer), format_many(substrate_buffer), truncation=False, padding=True)
+            
+            input_ids      = torch.tensor(ids['input_ids']).to(device)
+            attention_mask = torch.tensor(ids['attention_mask'], dtype=torch.bool).to(device)
+            
+            ############
+            model = model.to(device)
+            with torch.no_grad():
+                result    = model(input_ids=input_ids, attention_mask=attention_mask, output_hidden_states=output_hidden_states, output_attentions=output_attentions)
+
+            if output_hidden_states:
+                embedding = [i[j].cpu().numpy() for i, j in zip(result['hidden_states'][-1], attention_mask)]
+            else:
+                embedding = batch_size * [None]
+
+            if output_attentions:
+                attention = [i[:,j][:,:,j].cpu().numpy() for i, j in zip(result['attentions'][-1], attention_mask)]
+            else:
+                attention = batch_size * [None]
+            
+            tokens = [i[j].cpu().numpy() for i, j in zip(input_ids, attention_mask)]
+            pred = softmax(result['logits'].cpu(), axis=1)[:,1].numpy()
+            
+            ############
+            outputs = zip(kinase_buffer, substrate_buffer, pred, tokens, embedding, attention)
+            for _kinase, _substrate, _pred, _tokens, _embedding, _attention in outputs:
+                output = {
+                    'kinase'    : _kinase,
+                    'substrate' : _substrate,
+                    'pred'      : _pred,
+                    'tokens'    : _tokens,
+                }
+
+                if output_hidden_states:
+                    output['embedding'] =  _embedding
+                    
+                if output_attentions:
+                    output['attention'] =  _attention
+
+                if input_order == 'SK':
+                    output['token_labels'] = np.array(((len(_substrate) + 2)*['S']) + ((len(_kinase) + 2)*['K']))
+                    
+                if input_order == 'KS':
+                    output['token_labels'] = np.array(((len(_kinase) + 2)*['K']) + ((len(_substrate) + 2)*['S']))
+
+                yield output
+            
+            ##########
+            kinase_buffer = []
+            substrate_buffer = []
+            
+            ##########
+            elapsed = time.time() - start
+            if verbose:
+                sys.stderr.write(f'Progress: {n+1}, Elapsed: {elapsed:.2f} ({(1+n)/elapsed:.2f} / s)\r')
+    
+    if verbose:
+        sys.stderr.write('\n')
+
+def predict_one(kinase, substrate, **kwargs):
+    kinase_allowed = {'A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V'}
+    substrate_allowed = {'A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V','-'}
+    phosphosite_allowed = {'S','T','Y'}
+
+    assert type(kinase) == str
+    assert all(i in kinase_allowed for i in kinase)
+
+    assert type(substrate) == str
+    assert all(i in substrate_allowed for i in substrate)
+    assert len(substrate) == 11
+    assert substrate[5] in phosphosite_allowed
+    
+    return next(batch_job([kinase], [substrate], **kwargs))['pred']
+
+def predict_many(kinases, substrates, **kwargs):
+    kinase_allowed = {'A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V'}
+    substrate_allowed = {'A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V','-'}
+    phosphosite_allowed = {'S','T','Y'}
+    
+    for kinase in kinases:
+        assert type(kinase) == str
+        assert all(i in kinase_allowed for i in kinase)
+        
+    for substrate in substrates:
+        assert type(substrate) == str
+        assert all(i in substrate_allowed for i in substrate)
+        assert len(substrate) == 11
+        assert substrate[5] in phosphosite_allowed
+    
+    return np.array([i['pred'] for i in batch_job(kinases, substrates, **kwargs)])
+
---
+++ README.md
@@ -0,0 +1,128 @@
+# Introduction
+
+This repository contains the code and datasets for the manuscript "Phosformer: An interpretable Transformer model for kinase-specific phosphosite prediction".
+
+
+# Installing dependencies
+
+### Downloading this repository
+
+```
+# Download this repository
+git clone https://github.com/waylandy/phosformer
+
+cd phosformer
+```
+
+### Installing dependencies with `conda`
+
+If you do not have `conda` installed, you can download from the [Anaconda website](https://www.anaconda.com/).
+
+```
+conda env create -f environment.yaml
+conda activate phosformer
+```
+
+If you want to exit the environment:
+
+```
+conda deactivate
+```
+
+If you want to start over, you can also delete the `conda` environment:
+
+```
+conda env remove -n phosformer
+```
+
+**Notes on PyTorch:** The Phosformer model uses the PyTorch library which can benefit from GPU acceleration. You can monitor your GPU and check the your CUDA version by running `nvidia-smi -l 1`. PyTorch should always work on CPU, however additional steps may be required to enable CUDA, depending on your GPU model. For more information, see the installation guide on the [PyTorch website](https://pytorch.org/). 
+
+
+# Example code
+
+The section describes how to load Phosformer and use it for kinase-specific phosphosite predictions. Example code can be found in under `example.ipynb`. The computational notebook can be viewed using JupyterLab which is included in our environment. You can run it using `jupyter lab`.
+
+### Loading Phosformer
+
+The section describes how to load the model and tokenizer. If this is your first time running Phosformer, the model will automatically be downloaded from Hugging Face Hub and cached for subsequent uses.
+
+```
+import Phosformer
+
+# authentication tokens are provided for reviewers
+# this will be removed upon acceptance & the model will be made fully public
+auth_token = "hf_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+# load Phosformer model and tokenizer
+model = Phosformer.RobertaForSequenceClassification.from_pretrained('waylandy/phosformer', use_auth_token=auth_token)
+tokenizer = Phosformer.RobertaTokenizer.from_pretrained('waylandy/phosformer', use_auth_token=auth_token)
+
+# disables dropout for deterministic results
+model.eval()
+```
+
+### Running predictions on kinase-peptide pairs
+
+Phosformer generates predictions based on **two inputs**:
+
+1. a kinase domain sequence
+    - must be a string composed of the 20 amino acid, in uppercase
+    - the sequence should **not** contain gap characters
+    - the sequence can be any length (although kinase domains are usually ~300 residues long)
+    - sequences for all human kinases can be found under `data/human_kinases.csv`
+2. a peptide sequence
+    - must be a string composed of the 20 amino acid, in uppercase
+    - must be exactly 11 residues long
+    - the center residue is defined as the potential phosphosite and must be either S, T, or Y
+    - if the potential phosphosite occurs within 5 residues of the N or C-terminal, pad the sequence with gap characters
+
+For each pair of inputs, Phosformer generates **one output**:
+
+1. probability of phosphoryllation
+    - this is a value in the range (0, 1) where values greater than 0.5 are positive predictions
+
+Unlike previous methods in phosphosite prediction, Phosformer does not utilize kinase or substrate labels as input. Phosformer's understanding of the kinase and substrate is based soley off of primary sequence information. Consequently the Phosformer architecture is capable of representing any kinase-substrate combination — new labels do not need to be added to accommodate the addition of new kinases.
+
+Here is a basic example showing how to run a single prediction.
+
+```
+# Example prediction using ERK2 kinase domain (UniProt: P28482)
+kinase  = 'YTNLSYIGEGAYGMVCSAYDNVNKVRVAIKKISPFEHQTYCQRTLREIKILLRFRHENIIGINDIIRAPTIEQMKDVYIVQDLMETDLYKLLKTQHLSNDHICYFLYQILRGLKYIHSANVLHRDLKPSNLLLNTTCDLKICDFGLARVADPDHDHTGFLTEYVATRWYRAPEIMLNSKGYTKSIDIWSVGCILAEMLSNRPIFPGKHYLDQLNHILGILGSPSQEDLNCIINLKARNYLLSLPHKNKVPWNRLFPNADSKALDLLDKMLTFNPHKRIEVEQALAHPYL'
+peptide = 'LLKLASPELER'
+
+Phosformer.predict_one(kinase, peptide, model=model, tokenizer=tokenizer)
+```
+
+Here is the same example, except the kinase domain sequence is retrieved from an included csv file.
+
+```
+import pandas as pd
+
+kinases       = pd.read_csv('data/human_kinases.csv')
+kinase_domain = kinases[kinases['uniprot']=='P28482']['sequence'].item()
+peptide       = 'LLKLASPELER'
+
+Phosformer.predict_one(kinase_domain, peptide, model=model, tokenizer=tokenizer)
+```
+
+Here is a basic example showing how to run a single prediction.
+
+```
+import pandas as pd
+
+kinases       = pd.read_csv('data/example_input.csv')
+kinase_list   = kinases['kinase domain sequence'].values
+peptide_list  = kinases['peptide sequence'].values
+
+Phosformer.predict_many(
+    kinase_list, peptide_list,
+    model=model, tokenizer=tokenizer,
+    batch_size=20, # how many samples to load at once, if you're running out of memory, you can set this number lower
+    device='cpu',  # either "cpu" or "cuda"
+    threads=1      # specify how many threads to use, can help speed up if running on cpu
+)
+```
+
+
+
+
